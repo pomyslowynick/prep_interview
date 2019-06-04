390f9eb82219bb272f836f0ba25352425151b7a8
goran_visnjic <dark0mlet@gmail.com>
2019-06-04 23:58:15 +0100

    Solved easy challenge

--- two_sum_leetcode.py
+++ two_sum_leetcode.py
@@ -4,7 +4,8 @@ class Solution:
         j = 1
         for num in nums:
             for num2 in nums[j:len(nums)]:
-                if (num + num2) == target:
+                if ((num + num2) == target) and (j != i):
                     return [i ,j] 
                 j += 1
+            j = 0
             i += 1
